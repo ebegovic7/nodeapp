5c56047d99825fa60d259f9960c3c9b7a8fc7183
ebegovic7 <46750862+ebegovic7@users.noreply.github.com>
2019-02-12 14:11:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,15 @@
 const express = require('express');
 var randomstring = require("randomstring");
 
-// Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 var tekst = randomstring.generate();
 
-// App
 const app = express();
 app.get('/', (req, res) => {
  res.send(' Random string: ' + tekst);
 });
-
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
